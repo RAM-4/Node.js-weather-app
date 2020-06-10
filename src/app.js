02669efd0904de22e790d81694edfbdf2355fc7f
hamjgit <hamj@pm.me>
2020-06-10 16:25:05 +0200

    Fixing label errors #2

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.get('/about', ( req, res ) => {
 
 // Route to help
 app.get('/help', ( req, res ) => {
-    res.render( label.site.help, {
+    res.render( label.site.help.title, {
         title: label.site.help.title,
         message: label.site.help.message,
         name: label.author
